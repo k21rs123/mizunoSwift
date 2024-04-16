cf4017c9c186ff93c3c41be7a1da43c312f660f1
Mizuno Yoshiki <113609577+k21rs123@users.noreply.github.com>
2024-04-16 16:46:51 +0900

    create project practiceApp

---
+++ practiceApp/practiceApp/practiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceAppApp.swift
+//  practiceApp
+//
+//  Created by mizuno on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct practiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
