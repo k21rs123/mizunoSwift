8ba4a60b3dcfe92517c64fcb9e1056377dc74414
Mizuno Yoshiki <113609577+k21rs123@users.noreply.github.com>
2024-04-28 11:35:47 +0900

    update

--- practiceApp/practiceApp/View/CameraAppView.swift
+++ practiceApp/practiceApp/View/CameraAppView.swift
@@ -10,6 +10,24 @@ import SwiftUI
 struct CameraAppView: View {
     @State private var isActiveCamera = false
     @State private var image: UIImage?
+    @Environment(\.displayScale) private var displayScale
+
+    
+    @MainActor
+        func render(image:UIImage) -> UIImage? {
+            let renderer = ImageRenderer(content: sampleView(image:image))
+
+            renderer.scale = displayScale
+
+            return renderer.uiImage
+        }
+    
+    
+    private func sampleView(image:UIImage) -> some View {
+        Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
+        }
     
     var body: some View {
         VStack {
@@ -24,70 +42,77 @@ struct CameraAppView: View {
             }
             
             if let image = image {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
+                
+                sampleView(image: image)
+                
+                Button {
+                    if let render = render(image: image) {
+                        UIImageWriteToSavedPhotosAlbum(render, nil, nil, nil)
+                    }
+                } label: {
+                    Text("保存")
+                }
             }
             
             Spacer()
-            .navigationTitle("Camera")
-            .sheet(isPresented: $isActiveCamera) {
-                NavigationStack {
-                    VStack {
-                        CameraView(image: $image).ignoresSafeArea()
-                    }
-                    .toolbar {
-                        Button("Close", role: .cancel){
-                            isActiveCamera.toggle()
+                .navigationTitle("Camera")
+                .sheet(isPresented: $isActiveCamera) {
+                    NavigationStack {
+                        VStack {
+                            CameraView(image: $image).ignoresSafeArea()
+                        }
+                        .toolbar {
+                            Button("Close", role: .cancel){
+                                isActiveCamera.toggle()
+                            }
                         }
                     }
                 }
-            }
         }
     }
 }
 
 public struct CameraView: UIViewControllerRepresentable {
     @Binding private var image: UIImage?
-
+    
     @Environment(\.dismiss) private var dismiss
-
+    
     public init(image: Binding<UIImage?>) {
         self._image = image
     }
-
+    
     public func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     public func makeUIViewController(context: Context) -> UIImagePickerController {
         let viewController = UIImagePickerController()
         viewController.delegate = context.coordinator
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             viewController.sourceType = .camera
         }
-
+        
         return viewController
     }
-
+    
     public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 }
 
 extension CameraView {
     public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         let parent: CameraView
-
+        
         init(_ parent: CameraView) {
             self.parent = parent
         }
-
+        
         public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             if let uiImage = info[.originalImage] as? UIImage {
                 self.parent.image = uiImage
             }
             self.parent.dismiss()
         }
-
+        
         public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             self.parent.dismiss()
         }
